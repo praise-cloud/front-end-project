921eaf6aaad64e967d40937cc1f3952bc98aeb96
pr642 <pr642@kali>
2023-12-11 00:33:57 +0100

    pushing the responsive versions of the website

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// 
+// menu function
+const bar = document.querySelector('#bar');
+const close = document.querySelector('#close');
+const nav = document.querySelector('#navbar');
+// logic
+if(bar){
+    bar.addEventListener('click', ()=> {
+        nav.classList.add('active');
+    })
+}
+
+if(close){
+    close.addEventListener('click', ()=> {
+        nav.classList.remove('active');
+    })
+}
